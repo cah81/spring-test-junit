b37c154d595fa29378272ea856d5b30f5960e566
carlos Angulo <carlosenrique.angulo@bosonit.com>
2022-11-09 18:26:12 +0100

    terminado testing repository and begin testing service

--- src/test/java/com/example/demo/testrepository/StudentRepositoryTest.java
+++ src/test/java/com/example/demo/testrepository/StudentRepositoryTest.java
@@ -3,6 +3,7 @@ package com.example.demo.testrepository;
 import com.example.demo.student.Gender;
 import com.example.demo.student.Student;
 import com.example.demo.student.StudentRepository;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -16,9 +17,15 @@ class StudentRepositoryTest {
 
     @Autowired
     private StudentRepository underTest;
-    @BeforeEach
-    void setup(){
 
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @AfterEach
+    void tearDown() {
+        underTest.deleteAll();
     }
 
     @DisplayName("Valida si se selecciona email")
@@ -34,5 +41,19 @@ class StudentRepositoryTest {
         boolean expected = underTest.selectExistsEmail(email);
         //then
         assertThat(expected).isTrue();
+    }
+
+    @DisplayName("Valida si no se selecciona email")
+    @Test
+    void itShouldCheckIfStudentEmailDoesNotExists() {
+
+        //given
+        String email = "test@gmail.com";
+
+
+        //when
+        boolean expected = underTest.selectExistsEmail(email);
+        //then
+        assertThat(expected).isFalse();
     }
 }
